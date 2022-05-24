990f676079c091bbedf58a87d60cb0f602546262
Reza Ghahremani <developer.ghahremani@gmail.com>
2022-05-24 23:18:29 +0430

    read from useEffect

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import AddRoute from "components/AddRoute";
 import { MainLayout } from "components/layout";
@@ -7,10 +7,24 @@ import SocialRoutes from "components/SocialRoutes";
 import { getSocialRoute } from "api/socialRout";
 import { useI18Next } from "i18n";
 
-type Props = { socialRoutes: SocialRoute[] };
-const Settings = ({ socialRoutes = [], ...props }: Props) => {
+// type Props = { socialRoutes: SocialRoute[] };
+const Settings = () => {
+  // const Settings = ({ socialRoutes = [], ...props }: Props) => {
+  const [socialRoutes, setSocialRoutes] = useState<SocialRoute[]>([]);
   const { t } = useI18Next();
-  console.log(props);
+
+  useEffect(() => {
+    handleGetSocialNetworks();
+  }, []);
+
+  const handleGetSocialNetworks = async () => {
+    try {
+      const { data } = await getSocialRoute();
+      setSocialRoutes(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <MainLayout>
@@ -21,17 +35,17 @@ const Settings = ({ socialRoutes = [], ...props }: Props) => {
   );
 };
 
-export const getServerSideProps = async () => {
-  try {
-    const { data } = await getSocialRoute();
-    return {
-      props: { socialRoutes: data },
-    };
-  } catch (error) {
-    return {
-      props: { error: "erorr occured" },
-    };
-  }
-};
+// export const getServerSideProps = async () => {
+//   try {
+//     const { data } = await getSocialRoute();
+//     return {
+//       props: { socialRoutes: data },
+//     };
+//   } catch (error) {
+//     return {
+//       props: { error: "erorr occured" },
+//     };
+//   }
+// };
 
 export default Settings;

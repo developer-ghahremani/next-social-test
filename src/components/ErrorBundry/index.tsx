1a591c4c90ad32e4f7d054897035e49f2382828d
Reza Ghahremani <developer.ghahremani@gmail.com>
2022-05-24 19:19:52 +0430

    typescript error bundry

--- src/components/ErrorBundry/index.tsx
+++ src/components/ErrorBundry/index.tsx
@@ -1,28 +1,62 @@
-import React from "react";
-interface PropIntreface {
-  children: React.ReactNode;
+// interface PropIntreface {
+//   children: React.ReactNode;
+// }
+
+// class ErrorBoundary extends React.Component {
+//   constructor(props: PropIntreface) {
+//     super(props);
+//     this.state = { hasError: false };
+//   }
+
+//   static getDerivedStateFromError(error: any) {
+//     // Update state so the next render will show the fallback UI.
+//     return { hasError: true };
+//   }
+//   componentDidCatch(error: any, errorInfo: any) {
+//     // You can also log the error to an error reporting service
+//     console.log(error, errorInfo);
+//   }
+
+//   render() {
+//     if (this.state.hasError) {
+//       // You can render any custom fallback UI
+//       return <h1>Something went wrong.</h1>;
+//     }
+//     return this.props.children;
+//   }
+// }
+
+// export default ErrorBoundary;
+
+import React, { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
 }
 
-class ErrorBoundary extends React.Component {
-  constructor(props: PropIntreface) {
-    super(props);
-    this.state = { hasError: false };
-  }
+interface State {
+  hasError: boolean;
+}
 
-  static getDerivedStateFromError(error: any) {
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
-  componentDidCatch(error: any, errorInfo: any) {
-    // You can also log the error to an error reporting service
-    console.log(error, errorInfo);
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
   }
 
-  render() {
+  public render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <h1>Sorry.. there was an error</h1>;
     }
+
     return this.props.children;
   }
 }

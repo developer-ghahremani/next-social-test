da1d8891d82af910ced7d10937ab4fbe29744c93
Reza Ghahremani <developer.ghahremani@gmail.com>
2022-05-24 22:57:37 +0430

    defalur social networks

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,25 +1,21 @@
 import React, { useState } from "react";
 
-import AddOrEditRoute from "components/AddOrEditRoute";
 import AddRoute from "components/AddRoute";
-import { Collapse } from "@mui/material";
 import { MainLayout } from "components/layout";
-import { PlusIcon } from "components/icons";
 import SocialRoute from "models/SocialRoute.model";
 import SocialRoutes from "components/SocialRoutes";
 import { getSocialRoute } from "api/socialRout";
-import { useAppSelector } from "store";
 import { useI18Next } from "i18n";
 
 type Props = { socialRoutes: SocialRoute[] };
-const Settings = (props: Props) => {
+const Settings = ({ socialRoutes = [] }: Props) => {
   const { t } = useI18Next();
 
   return (
     <MainLayout>
       <p className="text-xl font-bold">{t("general.userSettings")}</p>
       <AddRoute />
-      <SocialRoutes socialRoutes={props.socialRoutes} />
+      <SocialRoutes socialRoutes={socialRoutes} />
     </MainLayout>
   );
 };
